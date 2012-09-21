f01502fec6fe49cd4f6d82e1e7ccabd81a08908d
JamesCodes <James.Codes@gmail.com>
2012-09-21 16:54:30 +0100

    Answers being populated

--- client/js/client.js
+++ client/js/client.js
@@ -7,13 +7,21 @@ var round = feeder.subscribe('rounds');
 function Game() {
 
 	this.name = "FB Game";
-	this.round = null;
+	this.round = new Round({ "track": "Jump" , "mp3": "http://cdn-preview-a.deezer.com/stream/a4e149e52e2ffdc4f057661b40ba7ee3-1.mp3", "answers": [ "Imagine", "Go your own way", "We are young", "Take a walk" ] });
 	
 	this.renderRound = function() {
-		console.log(this.round.details);
-		$(".container").html(
-			this.round.track
-		);
+
+		var answers = this.round.answers;
+		
+		// Add answers
+		for (var i = answers.length - 1; i >= 0; i--) {
+			$('.guess ul li:nth-child(' + (i + 1) + ') a').text(answers[i]);
+		};
+
+		console.log(this.round);
+		
+
+		//console.log(this.round.track);
 	};
 	
 	this.setup = function() {
@@ -28,7 +36,7 @@ function Game() {
 
 
 function Round(data) {
-	this.details = JSON.parse(data);
+	this.details = data;
 	this.mp3 = this.details.mp3;
 	this.answers = this.details.answers;
 	this.track = this.details.track;
