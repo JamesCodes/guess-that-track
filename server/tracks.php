a06d96a0a7aab24bad2eb1fcb5562ece2c294e7f
Graeme Maciver <graeme.maciver@cohaesus.co.uk>
2012-09-21 15:42:22 +0100

    round

--- server/tracks.php
+++ server/tracks.php
@@ -2,7 +2,19 @@
 
 $tracks = array(
 	0 => array("Juicy", '3616616'),
-	1 => array("Start me up", '4125128')
+	1 => array("Start me up", '4125128'),
+	2 => array("Jump", '4125128'),
+	3 => array("Good Time", '4125128'),
+	4 => array("We are young", '4125128'),
+	5 => array("Most nights", '4125128'),
+	6 => array("Take a walk", '4125128'),
+	7 => array("Go your own way", '4125128'),
+	8 => array("Heroes", '4125128'),
+	9 => array("Imagine", '4125128'),
+	10 => array("Radio", '4125128'),
+	11 => array("Untrust us", '4125128'),
+	12 => array("Fader", '4125128')
+
 );
 
 ?>
